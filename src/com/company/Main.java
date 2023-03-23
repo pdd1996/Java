06259470d18fdfe05743f338734686f57c3bffcd
paddy <pdd337816464@163.com>
2023-03-23 22:54:14 +0800

    增加 - 第一次上传

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.method.PrintText;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        PrintText printText = new PrintText();
+        printText.printHello();
+    }
+}
---
+++ src/com/company/method/PrintText.java
@@ -0,0 +1,8 @@
+package com.company.method;
+
+public class PrintText {
+
+    public void printHello(){
+        System.out.println("Hello, Java");
+    }
+}
